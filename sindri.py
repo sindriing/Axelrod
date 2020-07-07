3d58858e07b19020e1894ae62a8bebd05b405842
sindriing <sindriingolfsson@gmail.com>
2020-07-08 00:34:58 +0100

    minor changes

--- sindri.py
+++ sindri.py
@@ -12,30 +12,32 @@ import pickle
 
 
 two_state_players = []
+print("Finding Unique players")
 for p in FSMPlayer_generator(size = 2, max_wait = 3):
     two_state_players.append(p)
 unique_players =  find_unique_FSMs(two_state_players)
-print(len(unique_players))
+print("Number of players: ", len(unique_players))
 
-players = unique_players[1::5]
-print(len(players))
+players = unique_players
 
+iterations = 30
+modifier = 0.2
 w=5
 ft = lambda x: max(0, 1-w+w*x/len(players))
-# ft = lambda x: x**2
 # Moran Process
 mp = MoranProcess(
     players=players,
     turns=200,
-    modifier=0.0,
+    modifier=modifier,
     fitness_transformation=ft,
+    extra_statistics = True,
     births_per_iter=len(players)//20) # 5% of players die every round
 
 
-for i in trange(10):
+for i in trange(iterations):
     mp.next_step()
 
-with open('pickletest.pickle', 'wb') as f:
+with open(f'mp_tests/mp-{int(modifier*10)}.pickle', 'wb') as f:
     # Pickle the 'data' dictionary using the highest protocol available.
     pickle.dump(mp, f)
 
